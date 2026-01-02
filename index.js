67fc46ff771acde316615ce582eae128a2217223
Anıl Sevinç <contact@anilsevinc.net>
2026-01-02 16:15:58 +0300

    Görev 3a tamamlandı

--- index.js
+++ index.js
@@ -101,7 +101,18 @@ let ucetambolunenler,
 
 // 3a çözümü
 
-/* kodlar buraya */
+enkucuk = sayilar[0];
+enbuyuk = sayilar[0];
+
+for (let i = 1; i < sayilar.length; i++) {
+  if (sayilar[i] < enkucuk) {
+    enkucuk = sayilar[i];
+  }
+  if (sayilar[i] > enbuyuk) {
+    enbuyuk = sayilar[i];
+  }
+}
+
 
 // 3b çözümü:
 
